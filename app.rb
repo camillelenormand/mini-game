5a242b28c1f47aa6f0700af375380a61616fbca8
Camille Lenormand <camille.lenormand02@gmail.com>
2023-04-25 17:16:35 +0200

    add comments

--- app.rb
+++ app.rb
@@ -2,31 +2,36 @@ require 'bundler'
 require 'pry'
 
 Bundler.require
-# app.rb
+# Require all the gems in the Gemfile
+
 require_relative 'lib/game'
 require_relative 'lib/player'
 
-#binding.pry
-
+# Create two new players
 player1 = Player.new("Josiane")
 player2 = Player.new("José")
-puts
+
+# Display the current state of the players
 puts "----- Current state of the players -----"
 puts
 player2.show_state
 player1.show_state
 puts
+
+# Start the fight
 puts "⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️"
 puts "⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️ Let's fight! ⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️"
-puts "⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️"
+puts "⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️⚔️"
 puts
+
+# Start the loop until one of the players has no more lifepoints
 while player1.lifepoints > 0 && player2.lifepoints > 0
-player1.attacks(player2)
-if player2.lifepoints <= 0
-  break
-end
-player2.attacks(player1)
-if player1.lifepoints <= 0
-  break 
-end
+  player1.attacks(player2)
+  if player2.lifepoints <= 0
+    break
+  end
+  player2.attacks(player1)
+  if player1.lifepoints <= 0
+    break 
+  end
 end
