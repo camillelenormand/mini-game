d11a0f3fdaa6a370389b0cda370f8555037c7d1a
Camille Lenormand <camille.lenormand02@gmail.com>
2023-04-26 08:40:13 +0200

    update methods

--- lib/game.rb
+++ lib/game.rb
@@ -1,30 +1,68 @@
-# lib/game.rb
-class Game
+require_relative 'player'
+
+class Game < Player
   attr_accessor :human_player, :enemies
-  def initialize
-    @human_player
-    @enemies
-  end
   
-  def kill_player(recipient)
+  def initialize(human_player, enemies)
+    super(lifepoints, dead)
+    @human_player = human_player
+    @enemies = enemies
   end
 
-  def is_still_ongoing
+  def is_still_ongoing?
+    lifepoints > 0 && !enemies.empty?
   end
 
   def show_players
+    puts human_player.show_state
+    puts enemies.size
   end
 
   def menu
+    puts "Enter your choice"
+    puts
+    puts "a. Look for a better weapon"
+    puts "s. Recover"
+    puts
+    puts "Attack a player"
+    puts
+    enemies.each_with_index do |enemy, index|
+      puts "#{index} - #{enemy.show_state}"
+    end
+    puts
+    user_choice = gets.chomp.downcase
+    menu_choice(user_choice)
   end
 
-  def menu_choice
+  def menu_choice(user_choice)
+    while !enemies.empty? && enemies.any?(&:alive?) && human_player.alive? do
+      case user_choice
+      when "a"
+        human_player.search_weapon
+      when "s"
+        human_player.search_health_pack
+      when "0".."3"
+        enemy = enemies[user_choice.to_i]
+        human_player.attacks(enemy)
+        enemies.delete_at(user_choice.to_i) if enemy.dead?
+      else
+        puts "Invalid input. Please try again."
+        user_choice = gets.chomp().downcase
+      end
+    end
+    enemies_attack
+    end_game
   end
+  
 
   def enemies_attack
+    puts "Bots are now attacking!! 🤖🤖"
+    enemies.each do |enemy|  
+      enemy.attacks(human_player) if enemy.alive?
+    end
   end
 
-  def end
+  def end_game
+    human_player.alive? ? puts("----------- YOU WON----------- 😎😎😎") : puts("----------- GAME OVER ----------- 😥😥😥")
   end
-
 end
